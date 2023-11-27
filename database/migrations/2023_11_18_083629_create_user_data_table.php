f97c865066aefd6794fc67361e218edb08b41342
RezaPratika <m.rezapratika@gmail.com>
2023-11-27 11:21:20 +0700

    update

--- database/migrations/2023_11_18_083629_create_user_data_table.php
+++ database/migrations/2023_11_18_083629_create_user_data_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('image');
             $table->string('view');
             $table->string('comment');
+            $table->string('video');
             $table->timestamps();
         });
     }
